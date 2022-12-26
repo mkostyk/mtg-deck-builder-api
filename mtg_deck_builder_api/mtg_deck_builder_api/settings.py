621a4d249ce3645edb6db8d8c4e8689e7939db94
mich930 <m.kostyk22@gmail.com>
2022-12-26 20:06:22 +0100

    back to the future

--- mtg_deck_builder_api/mtg_deck_builder_api/settings.py
+++ mtg_deck_builder_api/mtg_deck_builder_api/settings.py
@@ -118,6 +118,10 @@ SWAGGER_SETTINGS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST')
     }
 }
 
