2faef884e99bf60bbba38830369ca05ec1563f8a
mich930 <m.kostyk22@gmail.com>
2022-12-26 19:11:16 +0100

    Deploy stuff once again

--- mtg_deck_builder_api/mtg_deck_builder_api/settings.py
+++ mtg_deck_builder_api/mtg_deck_builder_api/settings.py
@@ -113,16 +113,7 @@ SWAGGER_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
